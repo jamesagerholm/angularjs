c09ae8345c1b95f2df41bf0d8058e88a465ac153
James Agerholm <jamesag2000@gmail.com>
2017-03-24 15:22:44 +0000

    smartasssript.js

---
+++ smartassscript.js
@@ -0,0 +1,33 @@
+
+
+$(document).ready(function(){
+
+$(".list").hide('slow');
+
+$("p, h2, li").mouseover(function(){
+    $(this).css("color","blue");
+});  
+
+
+$("p, h2, li").mouseout(function(){
+    $(this).css("color","black");
+});
+
+$(".listbutton").click(function(){
+    $(".list").slideToggle("350");
+});
+$('#btn').on('click', function() {
+  $('#msg').append(' World');
+});
+
+// jQuery
+$('#btn').on('click', function() {
+  $('#msg').append(' World');
+});
+
+
+});
+
+
+
+
